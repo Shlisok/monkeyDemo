d24342c1c9573e758c4d0d6ffc08585673c9d3db
Shlisok <shlisok@163.com>
2023-01-31 11:52:32 +0800

    使用jquery试试

--- hgu.js
+++ hgu.js
@@ -1,28 +1,18 @@
 // ==UserScript==
 // @name         河地大自动教学评估
 // @namespace 	 shlisok
-// @version      0.1
+// @version      0.3
 // @description  河北地质大学教务系统自动教学评估
 // @author       shl
 // @icon         https://bkimg.cdn.bcebos.com/pic/6f061d950a7b02083188dae96ad9f2d3572cc871?x-bce-process=image/resize,m_lfit,w_536,limit_1
 // @match        *://202.206.144.35/*
 // @license      AGPL-3.0
+// @require      https://cdn.bootcss.com/jquery/2.2.1/jquery.js
 // @grant        none
 // ==/UserScript==
 (function () {
         'use strict';
-        // 选取table
-        var table = document.getElementById("tblView");
-
-        // 选取所有单选框
-        var radios = table.getElementsByTagName("input");
-
-        //遍历所有单选，并选中所有优秀的选项
-        for (var i = 0; i < radios.length; i++) {
-            if (radios[i].value === "5_1") {
-                radios[i].checked = true;
-            }
-        }
-
+        //jquery写法
+        $("#tblView input[value='5']").prop("checked", true);
     }
 )();
