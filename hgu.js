7c9d780e04de08f61b22f7b0cf9ec5d7ad9b41e9
Shlisok <shlisok@163.com>
2023-01-31 11:25:19 +0800

    测试

--- hgu.js
+++ hgu.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         河地大自动教学评估
 // @namespace 	 shlisok
-// @version      0.1
+// @version      0.2
 // @description  河北地质大学教务系统自动教学评估
 // @author       shl
 // @icon         https://bkimg.cdn.bcebos.com/pic/6f061d950a7b02083188dae96ad9f2d3572cc871?x-bce-process=image/resize,m_lfit,w_536,limit_1
@@ -11,6 +11,6 @@
 // ==/UserScript==
 (function () {
         'use strict';
-        console.log("开始测试")
+        console.log("开始编写")
     }
 )();
