b89b3469dc3ffc0108cac617515b4cb09dac722d
Shlisok <shlisok@163.com>
2023-01-31 13:45:30 +0800

    修复

--- hgu.js
+++ hgu.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         河地大自动教学评估
 // @namespace 	 shlisok
-// @version      1.3
-// @description  河北地质大学教务系统自动完成选择，尚未开发自动提交，问就是懒
+// @version      1.5
+// @description  河北地质大学教务系统教学评估，自动完成优秀选择，因为考虑到需要修改选项，以及避免引起不可控的麻烦，未开启自动提交。该脚本采用值校验，所以有出错的可能性，出错请截图发送至shlisok@163.com
 // @author       shl
 // @icon         https://bkimg.cdn.bcebos.com/pic/6f061d950a7b02083188dae96ad9f2d3572cc871?x-bce-process=image/resize,m_lfit,w_536,limit_1
 // @match        *://202.206.144.35/*
@@ -18,3 +18,6 @@
         $("#tblView input[value='6_1']").prop("checked", true);
     }
 )();
+
+
+
