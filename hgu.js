c9e6932d379a96faf3476f09ba2c3fdfb667799a
Shlisok <shlisok@163.com>
2023-01-31 11:22:38 +0800

    测试

--- hgu.js
+++ hgu.js
@@ -6,7 +6,7 @@
 // @author       shl
 // @icon         https://bkimg.cdn.bcebos.com/pic/6f061d950a7b02083188dae96ad9f2d3572cc871?x-bce-process=image/resize,m_lfit,w_536,limit_1
 // @match        *://202.206.144.35/*
-
+// @license      AGPL-3.0
 // @grant        none
 // ==/UserScript==
 (function () {
