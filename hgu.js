27670ce3a896335b9ccb09964eb741f03223a778
Shlisok <110590181+Shlisok@users.noreply.github.com>
2023-10-20 23:47:50 +0800

    新教务适配

--- hgu.js
+++ hgu.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         河地大自动教学评估
 // @namespace    shlisok
-// @version      1.5
-// @description  河北地质大学教务系统教学评估，自动完成打满分，因为考虑到需要修改选项，以及避免引起不可控的麻烦，未开启自动提交。该脚本采用值校验，所以有出错的可能性，出错请截图发送至shlisok@163.com
+// @version      1.6
+// @description  河北地质大学新教务系统教学评估，自动完成打5分，因为考虑到需要修改选项，以及避免引起不可控的麻烦，未开启自动提交。该脚本采用值校验，所以有出错的可能性，出错请截图发送至shlisok@163.com
 // @author       shl
 // @icon         https://bkimg.cdn.bcebos.com/pic/6f061d950a7b02083188dae96ad9f2d3572cc871?x-bce-process=image/resize,m_lfit,w_536,limit_1
 // @match        *://webvpn.hgu.edu.cn/*
